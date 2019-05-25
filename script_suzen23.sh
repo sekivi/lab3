3afea853b1c76445c14a8349c1f073563c79b9d4
Kirill Levkin <sekivi@mail.ru>
2019-05-25 11:18:00 +0300

    suzen23

---
+++ script_suzen23.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+for i in $(seq 1 5); do mv /home/suzen/destination/nginx$i.log /home/suzen/destination/nginx$i.log.back; done
+for i in $(seq 1 99); do mv /home/suzen/source/nginx$i.log /home/suzen/destination; done
